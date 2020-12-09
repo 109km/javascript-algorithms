6605de9dcbf8e6f72ea484e4b11d6576d46cc218
shaoxinheng <shaoxinheng@gmail.com>
2020-12-09 19:11:23 +0800

    up

--- advanced-algorithm/08-hash-table/hash-table.ts
+++ advanced-algorithm/08-hash-table/hash-table.ts
@@ -1,18 +1,24 @@
-class LinkedNode {
-  data: any
-  next: LinkedNode | null
+class HashLinkedNode {
+  value: any;
+  key: string;
+  next: HashLinkedNode | null;
 
-  constructor(data: any) {
-    this.data = data;
+  constructor(key: string, value: any) {
+    this.key = key;
+    this.value = value;
     this.next = null;
   }
 }
 
 class HashTableForCityNumber {
 
-  buckets: LinkedNode[];
+  buckets: HashLinkedNode[];
 
-  private bucketSize: number = 20;
+  private bucketSize: number = 1000;
+
+  constructor() {
+    this.buckets = [];
+  }
 
   // Treat the number as a 6-digit int.
   private hash(key: number): number {
@@ -20,21 +26,81 @@ class HashTableForCityNumber {
     return bucketNum;
   }
 
-  private loopLinkList(head: LinkedNode): void {
+  // Loop the link list to find the node.
+  private findNodeByKeyInBucket(head: HashLinkedNode, key: string): any {
     let current = head;
-    while (current.next !== null) {
+    let prev = null;
+    let node: HashLinkedNode = null;
 
+    while (current !== null) {
+      if (current.key === key) {
+        node = current;
+        break;
+      }
+      prev = current;
+      current = current.next;
+    }
+    return {
+      current: node,
+      prev: prev
+    };
+  }
+
+  put(key: string, value: any): void {
+    const bucketNum: number = this.hash(parseInt(key));
+    const bucketHead: HashLinkedNode = this.buckets[bucketNum];
+
+    // First node
+    if (typeof bucketHead === 'undefined') {
+      this.buckets[bucketNum] = new HashLinkedNode(key, value);
+    }
+    else {
+      const targetNode = this.findNodeByKeyInBucket(bucketHead, key);
+      // Insert the new node to head
+      if (targetNode.current === null) {
+        const insertNode = new HashLinkedNode(key, value);
+        insertNode.next = bucketHead;
+        this.buckets[bucketNum] = insertNode;
+      }
+      // Replace current value
+      else {
+        targetNode.current.value = value;
+      }
     }
   }
-
-  put(key: string | number, value: any): void {
-    const bucketNum: number = this.hash(parseInt(key.toString()));
-    this.buckets[bucketNum]
+  get(key: string): HashLinkedNode {
+    const bucketNum: number = this.hash(parseInt(key));
+    const bucketHead: HashLinkedNode = this.buckets[bucketNum];
+    const targetNode = this.findNodeByKeyInBucket(bucketHead, key).current;
+    return targetNode ? targetNode.value : null;
   }
-  get(key: string | number): any {
+  remove(key: string): HashLinkedNode {
+    const bucketNum: number = this.hash(parseInt(key));
+    const bucketHead: HashLinkedNode = this.buckets[bucketNum];
+    const targetNode = this.findNodeByKeyInBucket(bucketHead, key);
 
-  }
-  remove(key: string | number): void {
-
+    if (targetNode.current === null) {
+      throw new TypeError(`Can't remove a node which doesn't exist.`);
+    }
+    // Remove the head
+    else if (targetNode.prev === null) {
+      this.buckets[bucketNum] = targetNode.current.next;
+    }
+    // Remove node in the middle
+    else {
+      targetNode.prev.next = targetNode.current.next;
+    }
+    return targetNode.current;
   }
 }
+
+
+const cityHashTable = new HashTableForCityNumber();
+
+cityHashTable.put('110011', 'hello');
+cityHashTable.put('123116', 'world');
+
+console.log(cityHashTable.get('110011'));
+cityHashTable.remove('123116');
+cityHashTable.put('110011', 'hello2');
+console.log(cityHashTable.get('110011'));
