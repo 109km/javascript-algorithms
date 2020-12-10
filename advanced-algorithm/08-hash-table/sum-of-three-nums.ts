3860f9d2519576027718c704f096876f7bee3c79
shaoxinheng <shaoxinheng@gmail.com>
2020-12-10 19:51:52 +0800

    up

--- advanced-algorithm/08-hash-table/sum-of-three-nums.ts
+++ advanced-algorithm/08-hash-table/sum-of-three-nums.ts
@@ -10,23 +10,43 @@ const findThreeNumbersByTarget = (nums: number[], target: number): number[][] =>
 
   while (left < len - 2) {
     let sum = nums[left] + nums[mid] + nums[right];
+
+    // Minimum number is still bigger than target
+    if (nums[left] > target) {
+      break;
+    }
+
     if (sum === target) {
       res.push([nums[left], nums[mid], nums[right]]);
       mid++;
       right = mid + 1;
-      continue;
-    } else if (sum > target) {
-      left++;
-      mid = left + 1;
-      right = mid + 1;
-      continue;
-    } else {
+    }
+    else if (sum < target) {
+      // right reach the end
+      // move the mid
       if (right === len - 1) {
         mid++;
         right = mid + 1;
-      } else {
+        continue;
+      }
+
+      // mid reach the end
+      // move the left
+      if (mid === len - 2) {
+        left++;
+        mid = left + 1;
+        right = mid + 1;
+      }
+
+      // just move right
+      if (right < len - 1 && mid < len - 2) {
         right++;
       }
+    }
+    else {
+      left++;
+      mid = left + 1;
+      right = mid + 1;
     }
   }
 
