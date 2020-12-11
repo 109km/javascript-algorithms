ce52ec20c04eb48f84fddb5acbde17191561d6b1
shaoxinheng <shaoxinheng@gmail.com>
2020-12-11 14:37:11 +0800

    up

--- advanced-algorithm/08-hash-table/sum-of-three-nums.ts
+++ advanced-algorithm/08-hash-table/sum-of-three-nums.ts
@@ -1,60 +1,69 @@
 
-const findThreeNumbersByTarget = (nums: number[], target: number): number[][] => {
-  const res: number[][] = [];
+const findThreeNumbersByTarget = (nums: number[], target: number): Set<any> => {
+  const res = new Set();
+  const map = new Map();
   const len = nums.length;
   let left = 0;
   let mid = 1;
-  let right = 2;
 
   nums.sort((a, b) => (a - b));
 
-  while (left < len - 2) {
-    let sum = nums[left] + nums[mid] + nums[right];
+  // Put all nums into a set
+  // Time complexity is O(n)
+  for (let i = 0; i < len; i++) {
+    const num = nums[i];
+    if (typeof map.get(num) === 'undefined') {
+      map.set(num, [i]);
+    } else {
+      const indexs = map.get(num);
+      indexs.push(i);
+      map.set(num, indexs);
+    }
+  }
 
+  // Find the matched number by moving the two pointers,
+  // three numbers' sum can be seen as finding the match number
+  // equals target - num1 - num2.
+  while (left < len - 2) {
     // Minimum number is still bigger than target
     if (nums[left] > target) {
       break;
     }
-
-    if (sum === target) {
-      res.push([nums[left], nums[mid], nums[right]]);
-      mid++;
-      right = mid + 1;
-    }
-    else if (sum < target) {
-      // right reach the end
-      // move the mid
-      if (right === len - 1) {
-        mid++;
-        right = mid + 1;
-        continue;
-      }
-
-      // mid reach the end
-      // move the left
-      if (mid === len - 2) {
-        left++;
-        mid = left + 1;
-        right = mid + 1;
-        continue;
-      }
-
-      // just move right
-      if (right < len - 1 && mid < len - 2) {
-        right++;
-      }
-    }
-    else {
+    // mid number reach the end
+    if (mid === len - 2) {
       left++;
       mid = left + 1;
-      right = mid + 1;
+      continue;
+    }
+
+    let lastNum = target - nums[left] - nums[mid];
+    const matchedNums: number[] = map.get(lastNum);
+    // Can't find the match number,
+    // move the pointer.
+    if (typeof matchedNums === 'undefined') {
+      mid++;
+    }
+    // find the matched number
+    else {
+      matchedNums.forEach((matchNum: number) => {
+        // Don't add repeated combination
+        if (
+          left !== matchNum &&
+          mid !== matchNum &&
+          nums[left] !== nums[left - 1] &&
+          nums[mid] !== nums[mid - 1]
+        ) {
+          const combination = [nums[left], nums[mid], nums[matchNum]];
+          res.add(combination);
+        }
+      });
+      mid++;
     }
   }
-
   return res;
 }
 
-const nums: number[] = [1, 5, 9, 7, 7, 6, 0];
+const nums: number[] = [1, 0, -1, 5, 9, 7, 7, 6, 0, -1];
 console.log(findThreeNumbersByTarget(nums, 15));
 
 export { }
