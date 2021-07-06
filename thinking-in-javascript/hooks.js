2a53389f476a31512ae3a13a442f3389196ed6b7
shaoxinheng <shaoxinheng@gmail.com>
2021-07-06 16:15:53 +0800

    up

---
+++ thinking-in-javascript/hooks.js
@@ -0,0 +1,58 @@
+const ReactHooks = (() => {
+  const hooks = [] // store the states by order
+  let currentHookIndex = 0 // current hook index
+
+  return {
+    useState(initial) {
+      // index is stored in this closure
+      const index = currentHookIndex
+
+      // initialize, if there's already a state do nothing
+      if (typeof hooks[index] === 'undefined') {
+        hooks[index] = initial
+      }
+
+      // change the corresponding index's state
+      function setState(newState) {
+        if (newState !== hooks[index]) {
+          hooks[index] = newState
+        }
+      }
+
+      // move to the next index
+      currentHookIndex++
+
+      return [hooks[index], setState]
+    },
+    render(Component) {
+      const comp = Component()
+      comp.render()
+
+      // After rendering, reset the index.
+      currentHookIndex = 0
+      return comp
+    },
+  }
+})()
+
+function Count() {
+  const [num1, setNum1] = ReactHooks.useState(0)
+  const [num2, setNum2] = ReactHooks.useState(0)
+  return {
+    click: () => {
+      setNum1(num1 + 1)
+      setNum2(num2 + 2)
+    },
+    render: () => {
+      console.log('Count.render', { num1, num2 })
+    },
+  }
+}
+
+let comp1 = ReactHooks.render(Count)
+comp1.click()
+
+comp1 = ReactHooks.render(Count)
+comp1.click()
+
+comp1 = ReactHooks.render(Count)
