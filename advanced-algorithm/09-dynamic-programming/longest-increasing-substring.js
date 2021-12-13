0e526cdd97f9c1ae9da4f63cad3303975aaa49cf
shaoxinheng <shaoxinheng@gmail.com>
2021-12-13 19:04:45 +0800

    up

--- advanced-algorithm/09-dynamic-programming/longest-increasing-substring.js
+++ advanced-algorithm/09-dynamic-programming/longest-increasing-substring.js
@@ -1,6 +1,23 @@
-const getLongestIncreasingSubsequence = (numbers, longestSubsequence = []) => {
+const getLongestIncreasingSubsequence = (numbers) => {
   if (numbers.length === 0) return null
   if (numbers.length === 1) return numbers[0]
+
+  let longestSubsequence = []
+  for (let i = 0; i < numbers.length; i++) {
+    let start = numbers[i]
+    let solution = [start]
+    for (let j = i + 1; j < numbers.length; j++) {
+      let next = numbers[j]
+      if (next > solution[solution.length - 1]) {
+        solution.push(next)
+      }
+    }
+    if (solution.length > longestSubsequence.length) {
+      longestSubsequence = solution
+    }
+  }
+
+  return longestSubsequence
 }
 
 const arr = [10, 22, 9, 33, 21, 5, 41, 60]
