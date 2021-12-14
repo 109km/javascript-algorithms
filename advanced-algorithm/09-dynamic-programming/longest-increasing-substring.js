ac84cc4d5e8a2400a1172ccaa446773e8945a122
shaoxinheng <shaoxinheng@gmail.com>
2021-12-14 13:31:30 +0800

    up

--- advanced-algorithm/09-dynamic-programming/longest-increasing-substring.js
+++ advanced-algorithm/09-dynamic-programming/longest-increasing-substring.js
@@ -1,25 +1,59 @@
 // Brute force
-const getLongestIncreasingSubsequence = (numbers) => {
-  if (numbers.length === 0) return null
-  if (numbers.length === 1) return numbers[0]
+// time: O(n^2)
+// space: O(n)
+const countLongestIncreasingSubsequence = (numbers) => {
+  if (numbers.length <= 1) return numbers.length
 
-  let longestSubsequence = []
-  for (let i = 0; i < numbers.length; i++) {
-    let start = numbers[i]
-    let solution = [start]
-    for (let j = i + 1; j < numbers.length; j++) {
-      let next = numbers[j]
-      if (next > solution[solution.length - 1]) {
-        solution.push(next)
+  const dp = Array(numbers.length).fill(1)
+  const len = numbers.length
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < i; j++) {
+      if (numbers[i] > numbers[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1)
       }
     }
+  }
+  return Math.max(...dp)
+}
+
+const countLongestIncreasingSubsequenceWithMemo = (numbers) => {
+  if (numbers.length <= 1) return numbers.length
+
+  const dp = Array(numbers.length).fill(1)
+  const len = numbers.length
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < i; j++) {
+      if (numbers[i] > numbers[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1)
+      }
+    }
+  }
+  return Math.max(...dp)
+}
+
+const getLongestIncreasingSubsequence = (numbers) => {
+  if (numbers.length <= 1) return numbers
+  const dp = Array(numbers.length).fill([])
+  const len = numbers.length
+  for (let i = 0; i < len; i++) {
+    dp[i] = [numbers[i]] // dp[i] at least contains numbers[i]
+    for (let j = 0; j < i; j++) {
+      if (numbers[i] > numbers[j]) {
+        if (dp[i].length < dp[j].length + 1) {
+          dp[i] = [...dp[j], numbers[i]]
+        }
+      }
+    }
+  }
+  let longestSubsequence = []
+  dp.map((solution) => {
     if (solution.length > longestSubsequence.length) {
       longestSubsequence = solution
     }
-  }
-
+  })
   return longestSubsequence
 }
 
-const arr = [10, 22, 9, 20, 21, 5, 41, 60]
+const arr = [12, 100, 13, 11, 12, 13]
+console.log(countLongestIncreasingSubsequence(arr))
 console.log(getLongestIncreasingSubsequence(arr))
