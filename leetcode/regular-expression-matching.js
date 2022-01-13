bed27e66dd9a39177313c0af18246075b1551460
shaoxinheng <shaoxinheng@gmail.com>
2022-01-13 19:04:52 +0800

    up

--- leetcode/regular-expression-matching.js
+++ leetcode/regular-expression-matching.js
@@ -6,7 +6,7 @@
 
 const isMatch = (s, p) => {
   if (p.length === 0) return s.length === 0
-  if (p.length === 1) return s.length === 1 && (s[0] === p[0] || p[0] === '.')
+  if (p.length === 1) return s === p || p === '.'
 
   const matched = s.length > 0 && (s[0] === p[0] || p[0] === '.')
 
